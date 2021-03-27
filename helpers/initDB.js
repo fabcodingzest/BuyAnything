5938c10b23c0c2f726c22b27a6d028565f7fdaa4
fabcodingzest <fabrizvi786@gmail.com>
2021-03-28 01:44:00 +0530

    ugh

--- helpers/initDB.js
+++ helpers/initDB.js
@@ -1,18 +1,24 @@
 import mongoose from "mongoose";
 
 const initDB = async () => {
-  if (mongoose.connection.readyState >= 1) {
+  if (mongoose.connections[0].readyState >= 1) {
     console.log("Already Connected");
     return;
   }
   console.log(process.env.MONGO_URI);
-
-  return mongoose.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-  });
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    });
+    console.log("Connected");
+  } catch (e) {
+    console.log("-----------------------------------------------");
+    console.log(e);
+    console.log("-----------------------------------------------");
+  }
 };
 
 export default initDB;
