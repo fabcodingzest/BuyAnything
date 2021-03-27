c60c321a7d1d9e23f321daf46e6bcded72c79b0f
fabcodingzest <fabrizvi786@gmail.com>
2021-03-28 04:03:21 +0530

    change in irders.js

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -4,8 +4,6 @@ import Authenticated from "../../middleware/Authenticated";
 
 export default Authenticated(async (req, res) => {
   await initDB();
-  const orders = await Order.find({ user: req.userId }).populate(
-    "products.product"
-  );
+  const orders = await Order.find({ user: req.userId }).populate("products");
   res.status(200).json(orders);
 });
