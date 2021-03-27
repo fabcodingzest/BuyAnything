be7baba8db448ba4baf918965165447008795f2c
fabcodingzest <fabrizvi786@gmail.com>
2021-03-28 02:06:40 +0530

    idk bruh

--- pages/api/products.js
+++ pages/api/products.js
@@ -15,7 +15,7 @@ export default async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
+    const products = await Product.find();
     res.status(200).json(products);
   } catch (e) {
     res.status(500).json("Internal Server Error!");
