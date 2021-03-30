3735771bcbe34f576381c1627d39429a6f903a99
fabcodingzest <fabrizvi786@gmail.com>
2021-03-30 11:50:24 +0530

    solving bugs

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,9 +1,13 @@
 import Order from "../../models/Order";
+import Product from "../../models/Product";
 import initDB from "../../helpers/initDB";
 import Authenticated from "../../middleware/Authenticated";
 
 export default Authenticated(async (req, res) => {
   await initDB();
-  const orders = await Order.find({ user: req.userId }).populate("products.product");
+  const orders = await Order.find({ user: req.userId }).populate(
+    "products.product"
+  );
+  console.log(orders);
   res.status(200).json(orders);
 });
